690186b6140d3ac1b05633a39f01a57b58542e39
tanzhijin <tanzhijin@wckj.com>
2019-06-24 10:08:52 +0800

    de

--- .tea/entryFiles-production/index$.web.js
+++
@@ -1,8 +0,0 @@
-require('@alipay/appx-compiler/lib/sjsEnvInit');
-require('./config$');
-require('../../components/add-button/add-button');
-require('../../pages/index/index');
-require('../../pages/list/list');
-require('../../pages/item/item');
-require('../../pages/todos/todos');
-require('../../pages/add-todo/add-todo');
